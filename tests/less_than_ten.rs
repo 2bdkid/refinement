37de87f988b48d0a6bf714d8755675c9aef6c619
Brady Dean <2bdkid@gmail.com>
2021-01-19 20:34:08 -0500

    stashing unfinished changes

--- tests/less_than_ten.rs
+++ tests/less_than_ten.rs
@@ -1,6 +1,6 @@
 use refinement::{Predicate, Refinement};
 
-#[derive(PartialEq, Debug)]
+#[derive(PartialEq, PartialOrd, Eq, Ord, Debug)]
 struct LessThanTenPredicate;
 
 impl Predicate<i32> for LessThanTenPredicate {
@@ -16,4 +16,33 @@ fn add() {
     let x = LessThanTen::new(5).unwrap();
     let y = LessThanTen::new(5).unwrap();
     let _z = x + y;
+}
+
+#[test]
+fn assignment() {
+    let mut _x = LessThanTen::new(5).unwrap();
+    let y = LessThanTen::new(6).unwrap();
+    _x = y;
+    assert_eq!(6, _x.as_inner());
+}
+
+#[test]
+fn eq() {
+    let x = LessThanTen::new(5).unwrap();
+    let y = LessThanTen::new(5).unwrap();
+    assert_eq!(x, y);
+}
+
+#[test]
+fn neq() {
+    let x = LessThanTen::new(5).unwrap();
+    let y = LessThanTen::new(6).unwrap();
+    assert_ne!(x, y);
+}
+
+#[test]
+fn ord() {
+    let x = LessThanTen::new(5).unwrap();
+    let y = LessThanTen::new(6).unwrap();
+    assert!(x < y);
 }
